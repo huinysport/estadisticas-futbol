c7e6de0162945ce6042f4a39e3738296ef5ae606
huinysport <Huinysport@gmail.com>
2025-04-17 21:42:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Reemplaza con tu URL y tu clave de API
 const API_URL = 'https://v3.football.api-sports.io/fixtures';
-const API_KEY = 'fdb6b60c8cad45df1afb6c25a6fbbdaf
-';
+const API_KEY = 'fdb6b60c8cad45df1afb6c25a6fbbdaf';
 
 document.getElementById('loadData').addEventListener('click', async () => {
   try {
