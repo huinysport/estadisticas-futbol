078bb3861c9a84cafe1597911ae4e06b3ea80059
huinysport <Huinysport@gmail.com>
2025-04-18 00:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-document.getElementById('loadData').addEventListener('click', async () => {
-    try {
-        const API_URL = 'https://v3.football.api-sports.io/fixtures';
-        const API_KEY = 'fdb6b60c8cad45df1afb6c25a6fbbdaf';
+import http.client
 
-        const response = await fetch(API_URL, {
-            headers: { 'X-Auth-Token': API_KEY }
-        });
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
 
-        if (!response.ok) {
-            throw new Error(`Error al obtener datos: ${response.status}`);
-        }
-
-        const data = await response.json();
-        document.getElementById('loadData').addEventListener('click', async () => {
-    try {
-        const API_URL = 'https://v3.football.api-sports.io/fixtures';
-        const API_KEY = 'fdb6b60c8cad45df1afb6c25a6fbbdaf';
-
-        const response = await fetch(API_URL, {
-            headers: { 'X-Auth-Token': API_KEY }
-        });
-
-        if (!response.ok) {
-            throw new Error(`Error al obtener datos: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log(data); // Verifica los datos en la consola
-    } catch (error) {
-        console.error('Error:', error);
+headers = {
+    'x-rapidapi-host': "v3.football.api-sports.io",
+    'x-rapidapi-key': "fdb6b60c8cad45df1afb6c25a6fbbdaf"
     }
-}); {
-        console.error('Error:', error);
-    }
-});
+
+conn.request("GET", "/leagues", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
