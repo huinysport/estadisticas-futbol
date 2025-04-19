1ddf500426343c569e790768ee9bde53fb6798c5
huinysport <Huinysport@gmail.com>
2025-04-18 19:14:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ import Foundation
 import FoundationNetworking
 #endif
 
-var semaphore = DispatchSemaphore (value: 0)
+var semaphore = DispatchSemaphore(value: 0)
 
-var request = URLRequest(url: URL(string: "https://v3.football.api-sports.io/leagues")!,timeoutInterval: Double.infinity)
+var request = URLRequest(url: URL(string: "https://v3.football.api-sports.io/leagues")!, timeoutInterval: Double.infinity)
 request.addValue("fdb6b60c8cad45df1afb6c25a6fbbdaf", forHTTPHeaderField: "x-rapidapi-key")
 request.addValue("v3.football.api-sports.io", forHTTPHeaderField: "x-rapidapi-host")
-
 request.httpMethod = "GET"
 
 let task = URLSession.shared.dataTask(with: request) { data, response, error in
-  guard let data = data else {
-    print(String(describing: error))
+    guard let data = data else {
+        print(String(describing: error))
+        semaphore.signal()
+        return
+    }
+    
+    if let httpResponse = response as? HTTPURLResponse {
+        print("Código de estado: \(httpResponse.statusCode)")
+        
+        if httpResponse.statusCode == 200 {
+            print(String(data: data, encoding: .utf8)!)
+        } else {
+            print("Error: \(httpResponse.statusCode)")
+        }
+    }
+    
     semaphore.signal()
-    return
-  }
-  print(String(data: data, encoding: .utf8)!)
-  semaphore.signal()
 }
 
 task.resume()
 semaphore.wait()
+```
